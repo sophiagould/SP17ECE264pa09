df4dc44f4739fdf85559ce46c282932ccf4ed783
Sophia Alexandra Gould <gould7@ecegrid-thin1.ecn.purdue.edu>
2017-03-06 23:35:42 -0500

    first sub:

--- answer09.c
+++ answer09.c
@@ -1,6 +1,33 @@
 #include "answer09.h"
-
+#include "linkedlist.h"
 bool isCircular(const node_t * head)
 {
+	const node_t * sptr = head;
+	const node_t * fptr = head;
 
+	while (fptr != NULL){
+		if(sptr){
+			sptr = sptr->next;
+		}
+		else{
+			return false;
+		}
+		if(fptr){
+			fptr = fptr->next;
+		}
+		else{
+			return false;
+		}
+		
+		if(fptr){
+			fptr = fptr->next;
+		}
+		else{
+			return false;
+		}
+		if(sptr == fptr){
+			return true;
+		}
+	}
+	return false;
 }
